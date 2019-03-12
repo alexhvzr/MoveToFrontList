a7bc7db038d7938cf3510182497f6db5d0680593
Alex Hayden van Zuiden-Rylander <alex@alexs-mbp.lan>
2019-03-11 19:49:36 -0700

    First Commit

---
+++ cmake-build-debug/CMakeFiles/MTF_Transpose_List.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MTF_Transpose_List.dir/main.cpp.o"
+  "MTF_Transpose_List.pdb"
+  "MTF_Transpose_List"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MTF_Transpose_List.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
